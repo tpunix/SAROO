e3428a7d9ff9eaf45c51cb7ecfc8bc897828d9a4
tpu <tpunix@126.com>
2024-06-27 22:52:28 +0800

    更新language.c

--- Firm_Saturn/language.c
+++ Firm_Saturn/language.c
@@ -7,7 +7,6 @@
 int lang_id = 0;
 
 #define LANG_STR_NR 20
-#define LANG_NR 9
 
 typedef struct _str_entry
 {
@@ -17,14 +16,14 @@ typedef struct _str_entry
 	struct _str_entry *next;
 }STR_ENTRY;
 
-STR_ENTRY lang_str[LANG_STR_NR];
-STR_ENTRY *lang_str_table[64];
+static STR_ENTRY lang_str[LANG_STR_NR];
+static STR_ENTRY *lang_str_table[64];
 
 
 /******************************************************************************/
 
 
-char *lang_zhcn[LANG_STR_NR] = {
+static char *lang_zhcn[LANG_STR_NR] = {
 	"选择游戏(%d/%d)",
 	"选择文件(%d/%d)",
 	"游戏启动中......",
@@ -48,7 +47,7 @@ char *lang_zhcn[LANG_STR_NR] = {
 };
 
 
-char *lang_en[LANG_STR_NR] = {
+static char *lang_en[LANG_STR_NR] = {
 	"Select Game(%d/%d)",
 	"Select File(%d/%d)",
 	"Game Booting ......",
@@ -72,7 +71,7 @@ char *lang_en[LANG_STR_NR] = {
 };
 
 
-char *lang_ptbr[LANG_STR_NR] = {
+static char *lang_ptbr[LANG_STR_NR] = {
 	"Selecionar Jogo(%d/%d)",
 	"Selecionar Arquivo(%d/%d)",
 	"Inicializando Jogo ......",
@@ -96,7 +95,7 @@ char *lang_ptbr[LANG_STR_NR] = {
 };
 
 
-char *lang_ja[LANG_STR_NR] = {
+static char *lang_ja[LANG_STR_NR] = {
 	"ゲームリスト(%d/%d)",
 	"ファイルリスト(%d/%d)",
 	"ロード中......",
@@ -120,7 +119,7 @@ char *lang_ja[LANG_STR_NR] = {
 };
 
 
-char *lang_fr[LANG_STR_NR] = {
+static char *lang_fr[LANG_STR_NR] = {
 	"Choisir un jeu (%d/%d)",
 	"Choisir un fichier (%d/%d)",
 	"Démarrage du jeu...",
@@ -144,7 +143,7 @@ char *lang_fr[LANG_STR_NR] = {
 };
 
 
-char *lang_ru[LANG_STR_NR] = {
+static char *lang_ru[LANG_STR_NR] = {
 	"Выбор игры (%d/%d)",
 	"Выбор файла (%d/%d)",
 	"Запуск игры ...",
@@ -168,7 +167,7 @@ char *lang_ru[LANG_STR_NR] = {
 };
 
 
-char *lang_zhtw[LANG_STR_NR] = {
+static char *lang_zhtw[LANG_STR_NR] = {
 	"選擇遊戲(%d/%d)",
 	"選擇檔案(%d/%d)",
 	"啟動遊戲中......",
@@ -192,7 +191,7 @@ char *lang_zhtw[LANG_STR_NR] = {
 };
 
 
-char *lang_de[LANG_STR_NR] = {
+static char *lang_de[LANG_STR_NR] = {
 	"Spielauswahl(%d/%d)",
 	"Dateiauswahl(%d/%d)",
 	"Spiel wird geladen ......",
@@ -215,7 +214,7 @@ char *lang_de[LANG_STR_NR] = {
 	"Spielkategorie auswählen",
 };
 
-char *lang_es[LANG_STR_NR] = {
+static char *lang_es[LANG_STR_NR] = {
 	"Seleccionar juego(%d/%d)",
 	"Seleccionar archivo(%d/%d)",
 	"Cargando juego ......",
@@ -238,7 +237,8 @@ char *lang_es[LANG_STR_NR] = {
 	"Seleccionar categoría de juego",
 };
 
-char *lang_it[LANG_STR_NR] = {
+
+static char *lang_it[LANG_STR_NR] = {
 	"Seleziona gioco(%d/%d)",
 	"Seleziona file(%d/%d)",
 	"Caricamento gioco ......",
@@ -261,8 +261,8 @@ char *lang_it[LANG_STR_NR] = {
 	"Seleziona categoria di gioco",
 };
 
-
-char *lang_el[LANG_STR_NR] = {
+#if 0
+static char *lang_el[LANG_STR_NR] = {
 	"Επίλεξε Παιχνίδι(%d/%d)",
 	"Επίλεξε Αρχείο(%d/%d)",
 	"Εκκίνηση Παιχνιδιού ......",
@@ -283,10 +283,10 @@ char *lang_el[LANG_STR_NR] = {
 	"Δεν Βρέθηκε Δίσκος!",
 	"Δεν Είναι Δίσκος Παιχνιδιού!",
 	"Επίλεξε Κατηγορία Παιχνιδιού",
-}；
+};
 
 
-char *lang_ro[LANG_STR_NR] = {
+static char *lang_ro[LANG_STR_NR] = {
 	"Selectați jocul(%d/%d)",
 	"Selectați fişierul(%d/%d)",
 	"Jocul porneşte ......",
@@ -308,13 +308,31 @@ char *lang_ro[LANG_STR_NR] = {
 	"Nu s-a detectat un disc cu jocuri compatibil!",
 	"Selectați categoria de jocuri",
 };
+#endif
 
-char **lang_cur;
+static char **lang_list[] = {
+	lang_zhcn,
+	lang_en,
+	lang_ptbr,
+	lang_ja,
+	lang_fr,
+	lang_ru,
+	lang_zhtw,
+	lang_de,
+	lang_es,
+	lang_it,
+//	lang_el,
+//	lang_ro,    // TODO: Need new font
+};
+
+static const int lang_nr = sizeof(lang_list)/4;
+
+static char **lang_cur;
 
 
 /******************************************************************************/
 
-u32 str_hash(char *str)
+static u32 str_hash(char *str)
 {
 	int len = strlen(str);
 	u32 hash = 5381;
@@ -329,7 +347,7 @@ u32 str_hash(char *str)
 
 char *TT(char *str)
 {
-	if(lang_id==0 || lang_id>=LANG_NR)
+	if(lang_id==0 || lang_id>=lang_nr)
 		return str;
 
 	u32 hash = str_hash(str);
@@ -369,39 +387,18 @@ void lang_init(void)
 		//printk("%2d: %08x %s\n", i, lang_str[i].hash, lang_str[i].str);
 	}
 
-	lang_cur = NULL;
-	if(lang_id==1){
-		lang_cur = lang_en;
-	}else if(lang_id==2){
-		lang_cur = lang_ptbr;
-	}else if(lang_id==3){
-		lang_cur = lang_ja;
-	}else if(lang_id==4){
-		lang_cur = lang_fr;
-	}else if(lang_id==5){
-		lang_cur = lang_ru;
-	}else if(lang_id==6){
-		lang_cur = lang_zhtw;
-	}else if(lang_id==7){
-		lang_cur = lang_de;
-	}else if(lang_id==8){
-		lang_cur = lang_es;
-	}else if(lang_id==9){
-		lang_cur = lang_it;
-	}else if(lang_id==10){
-		lang_cur = lang_el;
-	}else if(lang_id==11){
-		lang_cur = lang_ro;
-	}
+	if(lang_id>=lang_nr)
+		lang_id = 0;
+	lang_cur = lang_list[lang_id];
 }
 
 
 void lang_next(void)
 {
 	lang_id += 1;
-	if(lang_id==LANG_NR)
+	if(lang_id==lang_nr)
 		lang_id = 0;
 
-	lang_init();
+	lang_cur = lang_list[lang_id];
 }
 
